0c15010e36c2e13b84371f3921f960dd372f2af8
sklyukin <stas.msu@gmail.com>
2016-06-03 11:55:20 +0500

    account transactions

--- src/services/user/AccountService.ts
+++ src/services/user/AccountService.ts
@@ -10,4 +10,17 @@ export class AccountService {
     return this.api.request('get', `accounts/${id}`)
       .map(data => data['amount']);
   }
+
+  payment(fromId, toId, amount) {
+    return this.api.request('post', 'accounts/payment', {fromId, toId, amount});
+  }
+
+  //user withdrew money
+  withdraw(userId, amount) {
+    return this.payment(userId, 0, amount);
+  }
+
+  getTransactions(userId) {
+    return this.api.request('get', `accounts/${userId}/transactions`);
+  }
 }
