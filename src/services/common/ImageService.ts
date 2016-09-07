432d80b4149dc584f9b473580e89090d8988e3bd
doctorMinami <ivan92zakharov@gmail.com>
2016-09-07 13:12:28 +0900

    ADD: method get in ImageService

--- src/services/common/ImageService.ts
+++ src/services/common/ImageService.ts
@@ -1,11 +1,17 @@
 import {Injectable} from '@angular/core';
 import {Config} from "../core/Config";
+import {ApiService} from "../core/ApiService";
 
 @Injectable()
 export class ImageService {
   IMAGE_UPLOAD_URL;
 
-  constructor() {
+  constructor(private api:ApiService) {
     this.IMAGE_UPLOAD_URL = Config.API_URL + 'images';
+  }
+
+  get({where, limit = 20, order = "", skip = 0, fields = {}}){
+    let filter = JSON.stringify({where: where, order: order, skip: skip, limit: limit, fields: fields});
+    return this.api.request('get', 'images',{filter:filter});
   }
 }
