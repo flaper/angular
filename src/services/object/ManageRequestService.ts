47e36920d3ac0a758d4262fb2d75411ec28d0957
HopeBetrayer <me.at.the.wind@gmail.com>
2016-10-13 15:45:30 +0300

    fixed count method

--- src/services/object/ManageRequestService.ts
+++ src/services/object/ManageRequestService.ts
@@ -16,11 +16,11 @@ export class ManageRequestService {
       //request objects
       .do(requests => this._object.requestIds(requests.map(r => r.subjectId)))
   }
-  count({where,limit = 0, order = "", offset = 0}) {
-    let filter = JSON.stringify({where: where,limit:limit, order: order, offset: offset});
-    return this.api.request('get', 'ManageRequests/count', {filet: filter});
+  count(where) {
+    let filter = JSON.stringify({where: where});
+    return this.api.request('get', 'ManageRequests/count', {filter: filter});
   }
-  
+
   getBySubjectId(subjectId) {
     return this.get({where: {subjectId: subjectId}}).map(data => data.length ? data[0] : null)
   }
