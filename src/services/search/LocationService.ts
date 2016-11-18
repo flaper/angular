0d26f80cdb302c3b17f869f2c09092d930c0a471
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-18 16:16:18 +0300

    resolved filenames error

--- src/services/search/LocationService.ts
+++ src/services/search/LocationService.ts
@@ -3,23 +3,6 @@ import {ApiService} from '../core/ApiService';
 
 @Injectable()
 
-export class SearchService {
-  config:any = {
-    host: "api.flaper.org",
-    port: "80"
-  };
-  constructor(private _api:ApiService) {
+export class LocationService {
 
-  }
-  search(query:string,region:string,domain:string):any {
-    let filter = {
-      where : {
-        query:query,
-      }
-    };
-    if (region) filter.where.region = region;
-    if (domain) filter.where.region = domain;
-        filterString = JSON.stringify(filter);
-    return this._api.request('get','Search',{filter:filterString});
-  }
 }
--- src/services/search/SearchService.ts
+++ src/services/search/SearchService.ts
@@ -1,9 +1,25 @@
 import {Injectable} from "@angular/core";
+import {ApiService} from '../core/ApiService';
 
 @Injectable()
 
-export class LocationService {
-  constructor() {
+export class SearchService {
+  config:any = {
+    host: "api.flaper.org",
+    port: "80"
+  };
+  constructor(private _api:ApiService) {
 
+  }
+  search(query:string="",region:string="",domain:string=""):any {
+    let filter = {
+      where : {
+        query:query,
+        region:region,
+        domain:domain
+      }
+    },
+    filterString = JSON.stringify(filter);
+    return this._api.request('get','Search',{filter:filterString});
   }
 }
